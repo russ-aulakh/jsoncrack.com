653002f766c57bb4318ca59d7170283e3a969464
russ.aulakh <russ.aulakh@gmail.com>
2025-08-18 08:54:19 -0400

    eslint fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,15 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function Home() {
-    const router = useRouter();
-    useEffect(() => {
-        router.replace("/editor");
-    }, [router]);
-    return (
-        <>
-            <noscript>
-                <meta httpEquiv="refresh" content="0; url=/editor" />
-            </noscript>
-        </>
-    );
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/editor");
+  }, [router]);
+  return (
+    <>
+      <noscript>
+        <meta httpEquiv="refresh" content="0; url=/editor" />
+      </noscript>
+    </>
+  );
 }
